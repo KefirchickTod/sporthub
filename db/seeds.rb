c20df9e1394b994b3a320b069f667988fa7741b1
zpyly <zpyly@softserveinc.com>
2022-01-13 17:52:57 +0200

    fix: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "faker"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Dir[File.join(Rails.root, "db", "seeders", "*.rb")].sort.each do |seed|
+
+  puts "Star seeding #{seed}"
+
+
+  begin
+    load seed
+  rescue
+    puts "Some error with seeding #{seed}"
+  end
+
+  sleep 0.2
+
+end
